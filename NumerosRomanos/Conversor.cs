c71421e4b8d9a056353e05edd22b97f4d2049355
Fabio Nascimento <fabio.nascimento@inventsoftware.com.br>
2014-03-04 18:36:10 -0300

    Refatoracao: Melhora no codigo de conversao e reducao de testes redundantes

---
+++ NumerosRomanos/Conversor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosRomanos
+{
+    public class Conversor
+    {
+        public int Converter(string numeroRomano)
+        {
+            var numeroIndoArabico   = 0;
+            var quantidadeV         = 0;
+            var quantidadeL         = 0;
+            var quantidadeD         = 0;
+            var quantidadeI         = 0;
+            var quantidadeX         = 0;
+
+            foreach (var letra in numeroRomano)
+            {
+                switch (letra)
+                {
+                    case 'V':
+                        quantidadeV++;
+                        break;
+                    case 'L':
+                        quantidadeL++;
+                        break;
+                    case 'D':
+                        quantidadeD++;
+                        break;
+                    case 'I':
+                        quantidadeI++;
+                        break;
+                    case 'X':
+                        quantidadeX++;
+                        break;
+                }
+                numeroIndoArabico += ObterValorLetra(letra);
+            }
+
+            if (quantidadeV > 1)
+                throw new Exception("Letra V deve repetir somente 1 vez !!");
+
+            if (quantidadeL > 1)
+                throw new Exception("Letra L não deve repetir!!");
+
+            if (quantidadeD > 1)
+                throw new Exception("Letra D não deve repetir");
+
+            if (quantidadeI > 3)
+                throw new Exception("Letra I nao pode repetir mais que 3 vezes");
+
+            if (quantidadeX > 3)
+                throw new Exception("Letra X nao pode repetir mais que 3 vezes");
+
+            return numeroIndoArabico;
+        }
+
+        public int ObterValorLetra(char numeroRomano)
+        {
+            switch (numeroRomano)
+            {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+            }
+        }
+
+    }
+}
---
+++ TesteNumerosRomanos/Teste_Conversao.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumerosRomanos;
+using NUnit.Framework;
+
+namespace TesteNumerosRomanos
+{
+    [TestFixture]
+    public class Teste_Conversao
+    {
+        [Test]
+        public void Deve_ser_possivel_realizar_conversao()
+        {
+            //Arrange
+            //Act
+            var conversor = new Conversor();
+
+            
+            //Assert
+            Assert.Pass();
+            
+        }
+
+        [Test]
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        [TestCase("X", 10)]
+        [TestCase("L", 50)]
+        [TestCase("C", 100)]
+        [TestCase("D", 500)]
+        [TestCase("M", 1000)]
+        public void Cada_letra_representa_um_valor(string letra, int resultadoEsperado)
+        {
+            //Arrange
+            var conversao = new Conversor();
+            //Act
+            var resultadoDaConversao = conversao.Converter(letra);            
+            //Assert
+            Assert.AreEqual(resultadoEsperado, resultadoDaConversao);
+
+            
+
+        }
+
+        [Test]
+        public void A_letras_V_NAO_podem_se_repetir()
+        {
+            var conversao = new Conversor();
+
+            Assert.DoesNotThrow(() => conversao.Converter("V"));
+
+            Assert.Throws<Exception>(() => conversao.Converter("VV"));
+            Assert.Throws<Exception>(() => conversao.Converter("VVVVVV"));
+            Assert.Throws<Exception>(() => conversao.Converter("VVVVVVVVV"));
+        }
+
+        [Test]
+        public void A_letras_L_NAO_podem_se_repetir()
+        {
+            var conversao = new Conversor();
+
+            Assert.DoesNotThrow(() => conversao.Converter("L"));
+
+            Assert.Throws<Exception>(() => conversao.Converter("LL"));
+
+        }
+
+        [Test]
+        public void A_letra_D_NAO_pode_repetir()
+        {
+            var conversao = new Conversor();
+            
+            Assert.DoesNotThrow(() => conversao.Converter("D"));
+
+            Assert.Throws<Exception>(() => conversao.Converter("DD"));
+        }
+
+        [Test]
+        public void A_letra_I_nao_pode_repetir_mais_que_tres_vezes()
+        {
+            var conversao = new Conversor();
+
+            Assert.DoesNotThrow(() => conversao.Converter("I"));
+            Assert.DoesNotThrow(() => conversao.Converter("II"));
+            Assert.DoesNotThrow(() => conversao.Converter("III"));
+
+            Assert.Throws<Exception>(() => conversao.Converter("IIII"));
+            Assert.Throws<Exception>(() => conversao.Converter("IIIIIIIIIIIIIIII"));
+            
+        }
+        
+        [Test]
+        public void A_letra_X_nao_pode_repetir_mais_que_tres_vezes()
+        {
+            var conversao = new Conversor();
+
+            Assert.Throws<Exception>(() => conversao.Converter("XXXX"));
+            Assert.Throws<Exception>(() => conversao.Converter("XXXXXXXXXXXXXXXXXXXXX"));
+        }
+        
+        [Test]
+        public void A_letra_X_pode_repetir_ate_tres_vezes()
+        {
+            var conversao = new Conversor();
+
+            Assert.DoesNotThrow(() => conversao.Converter("X"));
+            Assert.DoesNotThrow(() => conversao.Converter("XX"));
+            Assert.DoesNotThrow(() => conversao.Converter("XXX"));
+        }
+
+        
+
+    }
+}
