8aa06bfc7eb93b7cf1c368a7b17d79cd19f05ead
Fabio Nascimento <fabio.nascimento@inventsoftware.com.br>
2014-03-28 20:32:47 -0300

    Refatoracao e finalizacao da implementacao

--- NumerosRomanos/Conversor.cs
+++ NumerosRomanos/Conversor.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumerosRomanos
 {
     public class Conversor
     {
-        
+
         public int Converter(string numeroRomano)
         {
+            var numeroIndoArabico = 0;
+            var ultimoNumeroIndoArabico = 0;
+            var quantidadeLetra = new Dictionary<char, int>();
             var valorLetra = new Dictionary<char, int>
             {
                 {'I',1},
@@ -25,90 +22,56 @@ namespace NumerosRomanos
                 {'M',1000},
             };
 
-            var numeroIndoArabicoLetra = valorLetra['I'];
-            
-            var numeroIndoArabico = 0;
-            var quantidadeV = 0;
-            var quantidadeL = 0;
-            var quantidadeD = 0;
-            var quantidadeI = 0;
-            var quantidadeX = 0;
-            var ultimoNumeroIndoArabico = 0;
-
-            for (int i = numeroRomano.Length - 1; i >= 0; i--)
+            for (var i = numeroRomano.Length - 1; i >= 0; i--)
             {
                 var letra = numeroRomano[i];
 
-                switch (letra)
-                {
-                    case 'V':
-                        quantidadeV++;
-                        break;
-                    case 'L':
-                        quantidadeL++;
-                        break;
-                    case 'D':
-                        quantidadeD++;
-                        break;
-                    case 'I':
-                        quantidadeI++;
-                        break;
-                    case 'X':
-                        quantidadeX++;
-                        break;
-                }
+                if (!quantidadeLetra.ContainsKey(letra))
+                    quantidadeLetra.Add(letra, 0);
 
-                var numeroRepresentaLetra = ObterValorLetra(letra);
+                quantidadeLetra[letra]++;
+
+                var numeroRepresentaLetra = valorLetra[letra];
 
                 if (numeroRepresentaLetra < ultimoNumeroIndoArabico)
-                    numeroIndoArabico -= numeroRepresentaLetra;
-                else
-                    numeroIndoArabico += numeroRepresentaLetra;
-                
-                ultimoNumeroIndoArabico = numeroRepresentaLetra;
+                    numeroRepresentaLetra *= -1;
 
+                numeroIndoArabico += numeroRepresentaLetra;
+                ultimoNumeroIndoArabico = numeroRepresentaLetra;
             }
 
-            if (quantidadeV > 1)
-                throw new Exception("Letra V deve repetir somente 1 vez !!");
-
-            if (quantidadeL > 1)
-                throw new Exception("Letra L não deve repetir!!");
-
-            if (quantidadeD > 1)
-                throw new Exception("Letra D não deve repetir");
-
-            if (quantidadeI > 3)
-                throw new Exception("Letra I nao pode repetir mais que 3 vezes");
-
-            if (quantidadeX > 3)
-                throw new Exception("Letra X nao pode repetir mais que 3 vezes");
+            disparaSeHouverMaisQueUma(quantidadeLetra, "VLD");
+            disparaSeHouverMaisQueTres(quantidadeLetra, "IX");
 
             return numeroIndoArabico;
+
         }
 
-        public int ObterValorLetra(char numeroRomano)
+        private void disparaSeHouverMaisQueUma(Dictionary<char, int> quantidadeLetras, string letras)
         {
-            switch (numeroRomano)
+            foreach (var letra in letras)
             {
-                case 'I':
-                    return 1;
-                case 'V':
-                    return 5;
-                case 'X':
-                    return 10;
-                case 'L':
-                    return 50;
-                case 'C':
-                    return 100;
-                case 'D':
-                    return 500;
-                case 'M':
-                    return 1000;
-                default:
-                    return 0;
+                if (quantidadeLetras.ContainsKey(letra) && quantidadeLetras[letra] > 1)
+                {
+                    throw new Exception(
+                       string.Format(
+                           "Letra {0} não deve se repetir !!!", letra));
+                }
             }
+        }
 
+        private void disparaSeHouverMaisQueTres(Dictionary<char, int> quantidadeLetras, string letras)
+        {
+            foreach (var letra in letras)
+            {
+                if (quantidadeLetras.ContainsKey(letra) && quantidadeLetras[letra] > 3)
+                {
+                    throw new Exception(
+                        string.Format(
+                            "Letra {0} não deve se repetir !!!", letra));
+                }
+
+            }
         }
 
     }
